0d7c1ee9389ddd2dea1407be667de6526f580b34
Brian <bstanton773@gmail.com>
2023-10-23 14:33:20 -0500

    Add our first template!

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
+from flask import render_template
 
 # Create our first route
 @app.route('/')
 def index():
     name = 'Brian'
-    return '<h2>Hello World!!</h2>'
+    colors = ['red', 'orange', 'yellow', 'green', 'blue']
+    return render_template('index.html', first_name=name, last_name='Stanton', colors=colors)
 
 # Create a second route
 @app.route('/new')
