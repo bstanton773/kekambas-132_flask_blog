5debe0ef379513cb3d560dd41f648bd6474b197a
Brian <bstanton773@gmail.com>
2023-10-23 12:04:28 -0500

    Initial commit with Flask setup

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Create our first route
+@app.route('/')
+def index():
+    name = 'Brian'
+    return '<h2>Hello World!!</h2>'
+
+# Create a second route
+@app.route('/new')
+def new():
+    name = 'Brian Stanton'
+    return f"<h1>Hello {name}, this is a new route!</h1>"
