e534ff40e73cf32058a36963c361047bfc457b95
Brian <bstanton773@gmail.com>
2023-10-23 13:40:08 -0500

    Refactor into a Python Package

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+
+from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,4 @@
-from flask import Flask
-
-# Create an instance of the Flask class
-app = Flask(__name__)
+from app import app
 
 # Create our first route
 @app.route('/')
