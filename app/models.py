ff44f90c4f0a30e35cf394211a4d2a15f32b6388
Brian <bstanton773@gmail.com>
2023-10-26 16:21:18 -0500

    Add token and token_exp to User class

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 import random
+import os
+import base64
 from app import db, login
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -13,6 +15,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -23,6 +27,16 @@ class User(db.Model, UserMixin):
     
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
+
+    def get_token(self):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(hours=1)
+        db.session.commit()
+        return self.token
+        
 
 @login.user_loader
 def get_user(user_id):
