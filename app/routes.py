c8f87891f90de0fa6066f96ce4a356d624cebc77
Brian <bstanton773@gmail.com>
2023-10-26 12:25:53 -0500

    Handle Create Post Form submission to add to DB

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 # Import the SignUpForm, LoginForm, and PostForm classes from forms
 from app.forms import SignUpForm, LoginForm, PostForm
 # Import the User model from models
-from app.models import User
+from app.models import User, Post
 
 # Create our first route
 @app.route('/')
@@ -74,8 +74,22 @@ def logout():
     flash('You have successfully logged out')
     return redirect(url_for('index'))
 
-@app.route('/create-post')
+@app.route('/create-post', methods=["GET", "POST"])
 @login_required
 def create_post():
     form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        
+        # Create an instance of Post with form data and logged in user's ID
+        new_post = Post(title=title, body=body, user_id=current_user.id)
+        # Add to the database
+        db.session.add(new_post)
+        db.session.commit()
+
+        # flash a success message
+        flash(f"{new_post.title} has been created")
+        return redirect(url_for('index'))
+    
     return render_template('create_post.html', form=form)
