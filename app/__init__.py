aa63e022f55d951a03f31823e8ba4b00d7a91db2
Brian <bstanton773@gmail.com>
2023-11-15 15:01:39 -0600

    Updates for hosting

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 # Configure our app with the Attributes and Values from the Config class
 app.config.from_object(Config)
+
+# Add CORS to our app
+CORS(app)
 
 # Create an instance of SQLAlchemy to represent our database
 db = SQLAlchemy(app)
